8c400363b2e8388ad081d1af844aaa176d14b6de
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-09-18 14:21:34 -0300

    change to single quotes

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,16 +1,16 @@
-import { Context } from "../contexts/Context";
-import { OPEN_WEATHER_BASE_URL } from "../utils/openWeatherBaseURL";
+import { Context } from '../contexts/Context';
+import { OPEN_WEATHER_BASE_URL } from '../utils/openWeatherBaseURL';
 
-import axios from "axios";
-import { useContext } from "react";
-import { useQuery } from "react-query";
+import axios from 'axios';
+import { useContext } from 'react';
+import { useQuery } from 'react-query';
 
 export const useFetch = <T>(queryKey:string):[ T | undefined, boolean, boolean] => {
 
     const { state } = useContext(Context);
     const URL = OPEN_WEATHER_BASE_URL(queryKey);
 
-    const { data, isLoading, isError }= useQuery<T>([queryKey, state], 
+    const { data, isLoading, isError } = useQuery<T>([queryKey, state], 
 
         async () => {
 
