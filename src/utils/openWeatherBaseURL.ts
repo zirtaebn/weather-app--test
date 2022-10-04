4fae85a1b9b22b01ebd141ebed79ced24e38e282
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-04 13:03:55 -0300

    remove Context from openWeatherBaseURL file

--- src/utils/openWeatherBaseURL.ts
+++ src/utils/openWeatherBaseURL.ts
@@ -1,11 +1,7 @@
-import { Context } from '../contexts/Context';
+import { Context, initialStateType } from '../contexts/Context';
 
-import { useContext } from 'react';
-
-export const OPEN_WEATHER_BASE_URL = (parameter: string) => {
-
-    const { state } = useContext(Context);
-
+export const OPEN_WEATHER_BASE_URL = (parameter: string, state: initialStateType) => {
+    
     const URL = `${parameter}?lat=${state.adress.lat}&lon=${state.adress.lng}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&lang=${state.language.name}&units=${state.temp.isToggle ? 'metric' : 'imperial'}`;
 
     return URL
