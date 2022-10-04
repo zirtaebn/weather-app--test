4e0a37d1e9ca30589a44d48b0de928bf91b578b9
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-04 13:33:44 -0300

    change URL const to baseURL on api file

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,18 +1,18 @@
 import axios from 'axios';
 import { weatherDataType } from '../types/weatherDataType';
 
-const URL = 'https://api.openweathermap.org/data/2.5/';
+const baseURL = 'https://api.openweathermap.org/data/2.5/';
 
 const fetchWeatherData = async (requestParams:string): Promise<weatherDataType> => {
 
-    const response = await axios.get(`${URL}${requestParams}`);
+    const response = await axios.get(`${baseURL}${requestParams}`);
 
     return response.data
 }
 
 const fetchForecastData = async (requestParams:string) => {
 
-    const response = await axios.get(`${URL}${requestParams}`);
+    const response = await axios.get(`${baseURL}${requestParams}`);
 
     return response.data
 }
