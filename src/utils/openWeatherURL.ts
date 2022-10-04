8aecc50ba44e1da4193dd08a0292cffd40cd1943
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-04 14:05:29 -0300

    add open weather base URL to the openWeatherURL file

--- src/utils/openWeatherURL.ts
+++ src/utils/openWeatherURL.ts
@@ -2,5 +2,5 @@ import { initialStateType } from '../contexts/Context';
 
 export const openWeatherURL = (parameter: string, state: initialStateType) => {
     
-    return `${parameter}?lat=${state.adress.lat}&lon=${state.adress.lng}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&lang=${state.language.name}&units=${state.temp.isToggle ? 'metric' : 'imperial'}`;
+    return `https://api.openweathermap.org/data/2.5/${parameter}?lat=${state.adress.lat}&lon=${state.adress.lng}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&lang=${state.language.name}&units=${state.temp.isToggle ? 'metric' : 'imperial'}`;
 }
