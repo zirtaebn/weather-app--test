4340da4976f076a7d633ce647929c95767c93a34
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-08 15:31:59 -0300

    ajust index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
 import './index.css';
 import App from './App';
+import { queryClient } from './utils/queryClient';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from 'react-query';
-import { queryClient } from './utils/queryClient';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-
       <App />
-
     </QueryClientProvider>
   </React.StrictMode>
 );
