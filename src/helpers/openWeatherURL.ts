da0ce5295c69f417e024c0cd2c7cd806de3987c1
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-11-04 20:29:47 -0300

    remove helpers folder

--- src/helpers/openWeatherURL.ts
+++
@@ -1,6 +0,0 @@
-import { initialStateType } from '../contexts/Context';
-
-export const openWeatherURL = (parameter: string, state: initialStateType) => {
-    
-    return `https://api.openweathermap.org/data/2.5/${parameter}?lat=${state.adress.lat}&lon=${state.adress.lng}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&lang=${state.language.name}&units=${state.temp.isToggle ? 'metric' : 'imperial'}`;
-}
