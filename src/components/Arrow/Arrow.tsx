570c022a9ca2d33da59e5fce7af15b9dace1415f
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-11-08 12:29:17 -0300

    add inline background image to the Arrow component

--- src/components/Arrow/Arrow.tsx
+++ src/components/Arrow/Arrow.tsx
@@ -1,11 +1,20 @@
 import './Arrow.css';
+const arrow = require('../../assets/images/arrow.png');
 
 export const Arrow = () => {
 
     return(
 
-        <div className="arrow">
+        <div 
+            className="arrow" 
+            aria-label='Go back button' 
+            style = {{
 
+                backgroundImage: `url(${arrow})`,
+            
+            }}
+        >
+           
         </div>
     )
 }
