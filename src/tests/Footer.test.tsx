d0ddca9c566564be7930d4309c61a21d28cc38d2
Fabricio <fabricio_montagnani@hotmail.com>
2022-11-27 13:57:47 -0300

    test: fixed UT and introduced snapshot for Footer component

--- src/tests/Footer.test.tsx
+++ src/tests/Footer.test.tsx
@@ -1,68 +1,52 @@
-import { render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import { Footer } from '../components/Footer/Footer';
+import { renderWithContext } from '../helpers/testHelpers';
 
 
-it('it should have a "languages" div', () => {
-
-    const { container } = render (
-
-        <MemoryRouter>
+describe('[COMPONENTS]: Footer', () => {
+    it('it should render correctly', () => {
+        renderWithContext(
+          <MemoryRouter>
             <Footer />
-        </MemoryRouter>
-    );
+          </MemoryRouter>,
+        )
+    
+        expect(screen.getByLabelText('change the language to portuguese')).toMatchSnapshot()
+    });
+    
+    it('it should display footer message in Portuguese', async () => {
+        renderWithContext(
+            <MemoryRouter>
+              <Footer />
+            </MemoryRouter>,
+          )
+          userEvent.click(screen.getByLabelText('change the language to portuguese'))
 
-    expect(container.getElementsByClassName('languages').length).toBe(1);
-});
+        expect(await screen.findByText('Idioma selecionado: Português.')).toMatchSnapshot()
+    });
+    
+    it('it should display footer message in English', async () => {
+        renderWithContext(
+            <MemoryRouter>
+              <Footer />
+            </MemoryRouter>,
+          )
 
-it('it should have a "pt" div', () => {
+        userEvent.click(screen.getByLabelText('change the language to english'))
 
-    const { container } = render (
+        expect(await screen.findByText('Selected language: English.')).toMatchSnapshot()
+    });
+    
+    it('it should display footer message in Spanish', async () => {
+        renderWithContext(
+            <MemoryRouter>
+              <Footer />
+            </MemoryRouter>,
+          )
+          userEvent.click(screen.getByLabelText('change the language to spanish'))
 
-        <MemoryRouter>
-            <Footer />
-        </MemoryRouter>
-    );
-
-    expect(container.getElementsByClassName('pt').length).toBe(1);
-});
-
-it('it should have a "en" div', () => {
-
-    const { container } = render (
-
-        <MemoryRouter>
-            <Footer />
-        </MemoryRouter>
-    );
-
-    expect(container.getElementsByClassName('en').length).toBe(1);
-});
-
-it('it should have a "es" div', () => {
-
-    const { container } = render (
-
-        <MemoryRouter>
-            <Footer />
-        </MemoryRouter>
-    );
-
-    expect(container.getElementsByClassName('es').length).toBe(1);
-});
-
-
-it('it should have a "p" tag', () => {
-
-    const { container } = render (
-
-        <MemoryRouter>
-            <Footer />
-        </MemoryRouter>
-    );
-
-    expect(container.querySelector('p')).toBeInTheDocument;
-});
-
-
-export {};
+        expect(await screen.findByText('Idioma seleccionado: Español.')).toMatchSnapshot()
+    });
+})
